ebe1a27217e40b3f2423c92284ec24cfcd164162
pj-web <insta.pj.05@gmail.com>
2021-07-14 16:37:00 +0300

    parallax

--- dist/js/script.js
+++ dist/js/script.js
@@ -4,7 +4,54 @@ window.onload = function () {
     const parallax = document.querySelector('.hero');
 
     if (parallax) {
-        
+        const content = document.querySelector('.hero__container');
+        const clouds = document.querySelector('.images-hero__clouds');
+        const mountains = document.querySelector('.images-hero__mountains');
+        const human = document.querySelector('.images-hero__human');
+
+        // Коэффициенты
+        const forClouds = 40;
+        const forMountains = 20;
+        const forHuman = 10;
+
+        // Скорость анимации
+        const speed = 0.05;
+
+        // Объявление переменных 
+        let positionX = 0, positionY = 0;
+        let coordXprocent = 0, coordYprocent = 0;
+
+        function setMouseParallaxStyle() {
+            const distX = coordXprocent - positionX;
+            const distY = coordYprocent - positionY;
+
+            positionX = positionX + (distX * speed);
+            positionY = positionY + (distY * speed);
+
+            // Передаем стили
+            clouds.style.cssText = `transform: translate(${positionX / forClouds}%,${positionY / forClouds}%);`;
+            mountains.style.cssText = `transform: translate(${positionX / forMountains}%,${positionY / forMountains}%);`;
+            human.style.cssText = `transform: translate(${positionX / forHuman}%,${positionY / forHuman}%);`;
+
+            requestAnimationFrame(setMouseParallaxStyle);
+
+        }
+        setMouseParallaxStyle();
+
+        parallax.addEventListener("mousemove", function (e) {
+            // Получение ширины и высоты блока
+            const parallaxWidth = parallax.offsetWidth;
+            const parallaxHeight = parallax.offsetHeight;
+
+            // Ноль по середине
+            const coordX = e.pageX - parallaxWidth / 2;
+            const coordY = e.pageY - parallaxHeight / 2;
+
+            // Получаем проценты
+            coordXprocent = coordX / parallaxWidth * 100;
+            coordYprocent = coordY / parallaxHeight * 100;
+
+        });
     }
 }
 
