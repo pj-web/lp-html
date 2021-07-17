4c1dd39dbde9803512fb2f953d688cfd47983175
pj-web <insta.pj.05@gmail.com>
2021-07-17 14:26:16 +0300

    hero+s2-adaptive

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,82 +1,118 @@
 "use strict"
 
-window.onload = function () {
-    const parallax = document.querySelector('.hero');
+const isMobile = {
+    Android: function () {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function () {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function () {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function () {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function () {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function () {
+        return (
+                isMobile.Android()
+                || isMobile.BlackBerry()
+                || isMobile.iOS()
+                || isMobile.Opera()
+                || isMobile.Windows()
+                );
+    }
+};
 
-    if (parallax) {
-        const content = document.querySelector('.hero__container');
-        const clouds = document.querySelector('.images-hero__clouds');
-        const mountains = document.querySelector('.images-hero__mountains');
-        const human = document.querySelector('.images-hero__human');
+if (isMobile.any()) {
+    document.body.classList.add('_touch');
+} else {
+    document.body.classList.add('_pc');
 
-        // Коэффициенты
-        const forClouds = 40;
-        const forMountains = 20;
-        const forHuman = 10;
-
-        // Скорость анимации
-        const speed = 0.005;
-
-        // Объявление переменных 
-        let positionX = 0, positionY = 0;
-        let coordXprocent = 0, coordYprocent = 0;
-
-        function setMouseParallaxStyle() {
-            const distX = coordXprocent - positionX;
-            const distY = coordYprocent - positionY;
-
-            positionX = positionX + (distX * speed);
-            positionY = positionY + (distY * speed);
-
-            // Передаем стили
-            clouds.style.cssText = `transform: translate(${positionX / forClouds}%,${positionY / forClouds}%);`;
-            mountains.style.cssText = `transform: translate(${positionX / forMountains}%,${positionY / forMountains}%);`;
-            human.style.cssText = `transform: translate(${positionX / forHuman}%,${positionY / forHuman}%);`;
-
-            requestAnimationFrame(setMouseParallaxStyle);
-
-        }
-        setMouseParallaxStyle();
-
-        parallax.addEventListener("mousemove", function (e) {
-            // Получение ширины и высоты блока
-            const parallaxWidth = parallax.offsetWidth;
-            const parallaxHeight = parallax.offsetHeight;
-
-            // Ноль по середине
-            const coordX = e.pageX - parallaxWidth / 2;
-            const coordY = e.pageY - parallaxHeight / 2;
-
-            // Получаем проценты
-            coordXprocent = coordX / parallaxWidth * 100;
-            coordYprocent = coordY / parallaxHeight * 100;
-
-        });
-
-        // Parallax при скроле
-
-        let thresholdSets = [];
-        for (let i = 0; i <=1.0; i += 0.005) {
-            thresholdSets.push(i);
-        }
-        const callback = function (entries, observer) {
-            const scrollTopProcent = window.pageYOffset / parallax.offsetHeight * 100;
-            setParallaxItemsStyle(scrollTopProcent);
-        };
-        const observer = new IntersectionObserver(callback, {
-            threshold: thresholdSets
-        });
-
-        observer.observe(document.querySelector('.description'));
-
-        function setParallaxItemsStyle(scrollTopProcent) {
-            content.style.cssText = `transform: translate(0%,-${scrollTopProcent / 9}%);`;
-            // clouds.parentElement.style.cssText = `transform: translate(0%,-${scrollTopProcent / 10}%);`;
-            mountains.parentElement.style.cssText = `transform: translate(0%,-${scrollTopProcent / 6}%);`;
-            // human.parentElement.style.cssText = `transform: translate(0%,-${scrollTopProcent / 3}%);`;
+    window.onload = function () {
+        const parallax = document.querySelector('.hero');
+    
+        if (parallax) {
+            const content = document.querySelector('.hero__container');
+            const clouds = document.querySelector('.images-hero__clouds');
+            const mountains = document.querySelector('.images-hero__mountains');
+            const human = document.querySelector('.images-hero__human');
+    
+            // Коэффициенты
+            const forClouds = 40;
+            const forMountains = 20;
+            const forHuman = 10;
+    
+            // Скорость анимации
+            const speed = 0.005;
+    
+            // Объявление переменных 
+            let positionX = 0, positionY = 0;
+            let coordXprocent = 0, coordYprocent = 0;
+    
+            function setMouseParallaxStyle() {
+                const distX = coordXprocent - positionX;
+                const distY = coordYprocent - positionY;
+    
+                positionX = positionX + (distX * speed);
+                positionY = positionY + (distY * speed);
+    
+                // Передаем стили
+                clouds.style.cssText = `transform: translate(${positionX / forClouds}%,${positionY / forClouds}%);`;
+                mountains.style.cssText = `transform: translate(${positionX / forMountains}%,${positionY / forMountains}%);`;
+                human.style.cssText = `transform: translate(${positionX / forHuman}%,${positionY / forHuman}%);`;
+    
+                requestAnimationFrame(setMouseParallaxStyle);
+    
+            }
+            setMouseParallaxStyle();
+    
+            parallax.addEventListener("mousemove", function (e) {
+                // Получение ширины и высоты блока
+                const parallaxWidth = parallax.offsetWidth;
+                const parallaxHeight = parallax.offsetHeight;
+    
+                // Ноль по середине
+                const coordX = e.pageX - parallaxWidth / 2;
+                const coordY = e.pageY - parallaxHeight / 2;
+    
+                // Получаем проценты
+                coordXprocent = coordX / parallaxWidth * 100;
+                coordYprocent = coordY / parallaxHeight * 100;
+    
+            });
+    
+            // Parallax при скроле
+    
+            let thresholdSets = [];
+            for (let i = 0; i <=1.0; i += 0.005) {
+                thresholdSets.push(i);
+            }
+            const callback = function (entries, observer) {
+                const scrollTopProcent = window.pageYOffset / parallax.offsetHeight * 100;
+                setParallaxItemsStyle(scrollTopProcent);
+            };
+            const observer = new IntersectionObserver(callback, {
+                threshold: thresholdSets
+            });
+    
+            observer.observe(document.querySelector('.description'));
+    
+            function setParallaxItemsStyle(scrollTopProcent) {
+                content.style.cssText = `transform: translate(0%,-${scrollTopProcent / 9}%);`;
+                // clouds.parentElement.style.cssText = `transform: translate(0%,-${scrollTopProcent / 10}%);`;
+                mountains.parentElement.style.cssText = `transform: translate(0%,-${scrollTopProcent / 6}%);`;
+                // human.parentElement.style.cssText = `transform: translate(0%,-${scrollTopProcent / 3}%);`;
+            }
         }
     }
 }
+
+
+
 
 
 
@@ -194,38 +230,7 @@ contrySelect();
 
 
 
-const isMobile = {
-    Android: function () {
-        return navigator.userAgent.match(/Android/i);
-    },
-    BlackBerry: function () {
-        return navigator.userAgent.match(/BlackBerry/i);
-    },
-    iOS: function () {
-        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-    },
-    Opera: function () {
-        return navigator.userAgent.match(/Opera Mini/i);
-    },
-    Windows: function () {
-        return navigator.userAgent.match(/IEMobile/i);
-    },
-    any: function () {
-        return (
-                isMobile.Android()
-                || isMobile.BlackBerry()
-                || isMobile.iOS()
-                || isMobile.Opera()
-                || isMobile.Windows()
-                );
-    }
-};
 
-if (isMobile.any()) {
-    document.body.classList.add('_touch');
-} else {
-    document.body.classList.add('_pc');
-}
 
 const iconMenu = document.querySelector('.menu__icon');
 const menuBody = document.querySelector('.menu__body');
